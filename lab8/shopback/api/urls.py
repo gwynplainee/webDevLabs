0dc07e0e2b38a73808947aed3888059a29b52518
Ardak Abdullakhan <sjwiggrbrkwoscbejwi@gmail.com>
2025-04-05 13:34:11 +0600

    lab 8 models registered

--- lab8/shopback/api/urls.py
+++ lab8/shopback/api/urls.py
@@ -6,9 +6,9 @@ urlpatterns =[
     path('products/', views.get_products),
     path('products/create/', views.create_product),
     path('categories/', views.get_categories),
-    path('categories/create', views.create_category),
-    path('products/<int:id>', views.get_product_by_id),
-    path('categories/<int:id>', views.get_category_by_id),
+    path('categories/create/', views.create_category),
+    path('products/<int:id>/', views.get_product_by_id),
+    path('categories/<int:id>/', views.get_category_by_id),
     path('categories/<int:id>/products/', views.get_products_by_category)
     
 ]
